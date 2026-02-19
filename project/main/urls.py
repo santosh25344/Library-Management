ada1691036a9c6535ee1ef14938f89deee6c1213
santosh Shrestha <itcnepaltraders@gmail.com>
2026-02-19 09:45:08 +0545

    done

--- project/main/urls.py
+++ project/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard,name='dashboard'),
+    path('login/',views.login_view,name='login_view'),
+    path('signup/',views.signup_view,name='signup_view'),
+    
     
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     return render(request,'main/dashboard.html')
+
+def login_view(request):
+    return render(request,'main/login.html')
+
+def signup_view(request):
+    return render(request,'main/signup.html')
