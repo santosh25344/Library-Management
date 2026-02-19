4e5fba7f9d318b4774a37bf4ea4c8f8b79eebd2c
santosh Shrestha <itcnepaltraders@gmail.com>
2026-02-19 11:23:10 +0545

    update

--- project/main/urls.py
+++ project/main/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard,name='dashboard'),
-    path('login/',views.login_view,name='login_view'),
-    path('signup/',views.signup_view,name='signup_view'),
+    path('accounts/login/',views.login_view,name='login_view'),
+    path('accounts/signup/',views.signup_view,name='signup_view'),
     
     
 ]
