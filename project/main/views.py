e4a2e87260c1151df9b5c135a8cc5cb84be69f71
santosh Shrestha <itcnepaltraders@gmail.com>
2026-02-28 21:07:59 +0545

    done

--- project/main/views.py
+++ project/main/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.model import 
 
 @login_required
 def dashboard(request):
     return render(request,'main/dashboard.html')
 
+
 def login_view(request):
     return render(request,'main/login.html')
 
 def signup_view(request):
-    return render(request,'main/signup.html')
+    if request.method=='POST':
+        data=request.POST
+        username=data['username']
+        # user validation
+        if len(data[username<6]):
+            messages.error(request,'User Name must be 6 characters')
+            context={
+            'old_data': data,
+                }
+            return render(request,'main/signup.html')
+        # password match validation
+        if len(data[password])<6:
+            messages.error(request,'password must be 5 characters')
+            context={
+                'old_data': data,
+            
+            }
+            return render(request,'main/signup.html',context)
+        else:
+            if  data[password1]
+            
+            
+        
+            
+        
+    return render(request,'main/signup.html', context)
--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from os import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT='/static/'
+STATICFILES_DIRS=[
+    os.path.join('BASE_DIR','static')
+    ]
+
+
